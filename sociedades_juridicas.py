0e97ca5f5fa2ecce872fefb7b140a930edc5e83e
Lucas Ale <lucas_m_ale@hotmail.com>
2021-11-11 09:22:55 -0300

    Todo acomodado como es y comentado

--- sociedades_juridicas.py
+++ sociedades_juridicas.py
@@ -5,20 +5,16 @@ import glob
 
 #obtener el working directory
 path = os.getcwd()
+#buscar todos los archivos de extension csv y nombre con registro-nacional-sociedades
 all_files = glob.glob(path + "/registro-nacional-sociedades*.csv")
 print(all_files)
 df = pd.DataFrame()
 
-for i in range(len(all_files)):
-df1 = pd.read_csv(all_files[i], index_col=None, header=0)
-print(all_files[i])
-df1_sliced = df1.loc[df1['dom_fiscal_localidad'] == 'GENERAL PICO']
-df = df.append(df1_sliced)
-df = df.drop_duplicates()
+#Leer el csv sin encabezados
 df1 = pd.read_csv(all_files[-1], index_col=None, header=0)
-
+#Filtro domicilio fiscal en general pico
 df1_sliced = df1.loc[df1['dom_fiscal_localidad'] == 'GENERAL PICO']
-
+#Eliminar duplicados
 df1_sliced = df1_sliced.drop_duplicates()
-
+#Exportar csv
 df1_sliced.to_csv('registro-nacional-sociedades-general-pico.csv', index = False)
